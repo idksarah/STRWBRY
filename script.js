75dcf84dcf903ed59a54f0267c93c099467dd819
idksarah <sarah.p.ngai@gmail.com>
2024-06-08 10:49:59 -0700

    Version 1.00

---
+++ script.js
@@ -0,0 +1,12 @@
+//may have to link this later
+
+async function afterHighlight() {
+    //convert to lowercase
+    //compare against a dictionary
+    // if word matches dictionary, this{
+    //use the audio file from a dictionary and display definition
+    //remove when mouse moves away}
+    //otherwise, nothing happens
+}
+
+alert("hai!");
