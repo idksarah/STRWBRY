a1107a4b40f3aad83b90abf3d9554d91e2490ece
idksarah <sarah.p.ngai@gmail.com>
2024-06-24 13:23:10 -0700

    This one might be broekn tbh

---
+++ STRWBRY-Homepage/script.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('button');
+
+let thankYou = document.querySelector('#thankYou');
+let likeActually = document.querySelector('likeActually');
+
+button.addEventListener('click', function() {
+
+})
